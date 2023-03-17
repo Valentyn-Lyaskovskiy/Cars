4c3eed725438d034512e558a051541fb6e32c392
Valentyn Lyaskovskiy <riodemouro2017@gmail.com>
2023-03-17 14:14:02 +0200

    .

--- js/script.js
+++ js/script.js
@@ -28,6 +28,7 @@ const menuBtn = document.querySelector('.menu__btn');
 const menu = document.querySelector('.menu__list');
 menuBtn.addEventListener('click', function () {
   menu.classList.toggle('menu__list_active')
+  menuBtn.classList.toggle('menu__btn_active')
 })
 
 const swiper = new Swiper(".swiper", {
